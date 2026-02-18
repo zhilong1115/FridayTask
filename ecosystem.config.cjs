772cc1ddf84a218c1792806c5991ec4339655138
zhilong1115 <zhilong1115@users.noreply.github.com>
2026-02-17 19:43:01 -0800

    Add graceful shutdown + pm2 restart config to prevent port conflicts

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,11 @@ module.exports = {
     script: 'server/index.js',
     env: {
       FRIDAY_ADMIN_PASSWORD: 'ilovefriday'
-    }
+    },
+    kill_timeout: 5000,
+    wait_ready: false,
+    max_restarts: 10,
+    min_uptime: 5000,
+    restart_delay: 2000,
   }]
 };
